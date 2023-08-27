41584320b651528a9615d838f8873dc068315fd6
Vlad Cos <vcosvic@gmail.com>
2023-08-27 22:11:08 +0300

    WIP

--- .projenrc.ts
+++ .projenrc.ts
@@ -3,10 +3,6 @@ import { CustomJsii } from '@vladcos/projen-base'
 const project = new CustomJsii({
   name: '@vladcos/projen-github-action',
   repositoryUrl: 'https://github.com/vladcos/projen-github-action',
-  deps: ['@vladcos/projen-base@../projen-base'],
-  peerDeps: ['projen@0.72.19'],
 })
 
-// project.addDeps('zod-to-json-schema', 'zod')
-// project.addBundledDeps('zod-to-json-schema', 'zod')
 project.synth()
